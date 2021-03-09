c526eac6206c08887b843400fd1e71fb287f4981
Sai Sree Harsha <saisreeharsha@Sais-MacBook-Air.local>
2021-03-09 11:48:51 +0530

    modified test

--- test.py
+++ test.py
@@ -5,26 +5,46 @@ from member import Member
 g = Group(id=1)
 
 # Create memebers
-m1 = Member(id='Alice', secret_key=23)
-m2 = Member(id='Bob', secret_key=37)
+admin = Member(id='Alice', secret_key=23)
+member1 = Member(id='Bob', secret_key=37)
+member2 = Member(id='John', secret_key=71)
+member3 = Member(id='Tom', secret_key=79)
 
 # Add members
-g.add_member(m1)    # First member to the group has to be added directly, and that person will be the admin
-m1.add_member_to_group(m2, g)   # Subsequent new memberships to the group have to be done through the admin of the group 
+# First member to the group has to be added directly, and that person will be the admin
+g.add_member(admin)
 
-m1.remove_member_from_group(m1, g) # should not be possible becuse admin cant be removed
-m1.remove_member_from_group(m2, g) # should be possible
+# Subsequent new memberships to the group have to be done through the admin of the group 
+admin.add_member_to_group(member1, g)  
+admin.add_member_to_group(member2, g)  
 
-m1.add_member_to_group(m2, g)
+# Not possible as other members of the group, apart from the admin 
+# cannot add new members to the group
+member1.add_member_to_group(member3, g)
 
-# Get members list
+# Admin can remove members from the group
+admin.remove_member_from_group(member2, g)
+
+# Not possible as other members of the group, apart from the admin 
+# cannot remove any member of the group 
+member1.remove_member_from_group(admin, g) 
+
+# Admin of the group cannot be removed
+admin.remove_member_from_group(admin, g) 
+
+# Print all the members in the group
 g.print_members_list()
-
 
 # Writing a message to the group
 message = "Attack at midnight"
-m1.add_message_to_group(g, message)
-m1.read_latest_message_of_group(g)
-m2.read_latest_message_of_group(g)
+
+# A member can send a message to the group
+member1.add_message_to_group(g, message)
+
+# Any other member of the group can read the message
+admin.read_latest_message_of_group(g)
+
+# As member3 is not part of the group, message cant be read by member3
+member3.read_latest_message_of_group(g)
 
 
