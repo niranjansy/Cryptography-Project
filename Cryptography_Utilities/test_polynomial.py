1cef71df1bbe79c0c322a3e0dfb15e8fa7ac972d
sreesai1412 <sreesai1412@gmail.com>
2021-03-09 13:49:35 +0530

    crypto utils for polynomials

--- Cryptography_Utilities/test_polynomial.py
+++ Cryptography_Utilities/test_polynomial.py
@@ -1,7 +1,7 @@
 from encrypt_polynomial import encrypt_polynomial
 from decrypt_polynomial import decrypt_polynomial
 
-key = 57
+key = 89
 polynomial = [45, -65, 72, 5]
 encrypted_str = encrypt_polynomial(polynomial, key)
 decrypted_polynomial = decrypt_polynomial(encrypted_str, key)
